65c50731b2ddad4896eb414f764dbfb097ec1397
ketandabhi1710 <ketandabhi.1212@gmail.com>
2023-11-30 12:16:11 +0530

    added demo reoutes for testing

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ app.use(bodyParser.json());
 
 // User.sync({ alter: true });
 
-app.get("/", function (req, res) {
-  console.log("----from demo url---");
+app.get("*", function (req, res) {
+  console.log("----from the * url---");
+  res.json({ u: "Hello from one" });
+});
+
+
+app.get("/qqq", function (req, res) {
+  console.log("----from qqq url---");
   res.send("Hello World");
+});
+
+app.get("/one", function (req, res) {
+  console.log("----from the one url---");
+  res.json({ a: "Hello from one" });
+});
+
+app.get("/two", function (req, res) {
+  console.log("----from the two url---");
+  res.json({ b: "Hello from two" });
 });
 
 app.listen(process.env.PORT, () => {
